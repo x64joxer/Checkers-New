fb353b18906d9be1ba876777888ed954895a6f04
Roman Nowak <roman.nowak.1984@gmail.com>
2016-03-07 23:43:09 +0100

    Add message queue

--- Schelduer/Scheduler.cpp
+++ Schelduer/Scheduler.cpp
@@ -3,17 +3,25 @@
 Scheduler::Scheduler()
 {
     Traces() << "\n" << "LOG: Scheduler::Scheduler()";
+    messageQueue = new SharedPtrSet<boost::array<char, 8192> >;
 }
 
 void Scheduler::NewConnection(TCPConnection_ptr wsk)
 {
     Traces() << "\n" << "LOG: Adding new connection";    
 
-    connections_.PushBack(wsk);
+    wsk->SetMessageQueue(messageQueue);
+    connections_.insert(wsk);
     wsk->Start();
 }
 
 void Scheduler::Init()
 {
 
+}
+
+Scheduler::~Scheduler()
+{
+    Traces() << "\n" << "LOG: Scheduler::~Scheduler()";
+    delete messageQueue;
 }
--- Schelduer/Scheduler.h
+++ Schelduer/Scheduler.h
@@ -13,12 +13,14 @@ class Scheduler
         Scheduler();
         void Init();
         void NewConnection(TCPConnection_ptr wsk);
+        ~Scheduler();
 
     private:       
         TCPConnection *socket;
 
-        // The managed connections.
-        SharedPtrSet<TCPConnection_ptr> connections_;        
+        // The managed connections.        
+        std::set<TCPConnection_ptr> connections_;
+        SharedPtrSet<boost::array<char, 8192> > *messageQueue;
 };
 
 #endif // SCHEDULER_H
