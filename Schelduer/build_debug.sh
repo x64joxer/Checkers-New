6e17568f6fe0416a5775ec31259c336ef6a4462d
Roman Nowak <roman.nowak.1984@gmail.com>
2016-08-08 18:08:33 +0200

    Debug build added to scheduler

---
+++ Schelduer/build_debug.sh
@@ -0,0 +1,26 @@
+clear
+mkdir -p ../build-Scheduler
+echo Building...
+
+g++ -g main.cpp \
+    ConnectionManager/ConnectionManager.cpp \
+    Scheduler/Scheduler.cpp \
+    ServerState/ServerState.cpp \
+    Scheduler/MessageCoder.cpp \
+    Scheduler/Client.cpp \
+    Scheduler/Worker.cpp \
+    Peers/Peers.cpp \
+    Board/Board.cpp \
+    Board/Pawn.cpp \
+    Board/PawnPos.cpp \
+    Board/ThreadIASimpleBoard.cpp \
+    TCP/TCPConnection.cpp \
+    TCP/TCPServer.cpp \
+    SafeQueue/SharedMap/SharedMap.cpp \
+    SafeQueue/SharedPtrSet/SharedPtrSet.cpp \
+    SafeQueue/SharedPtrSet/SharedPtrList.cpp \
+    Traces/Traces.cpp \
+    -I /opt/local/include/ -L/opt/local/lib -std=c++11 -lboost_system -Wl,-rpath,/opt/local/lib -o ../build-Scheduler/scheduler -pthread
+
+
+echo ...end of building
