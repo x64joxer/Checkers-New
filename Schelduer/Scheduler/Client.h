b2cb9302d9e4452ff20a758cdb01c1d64b0908df
Roman Nowak <roman.nowak.1984@gmail.com>
2016-09-07 17:08:27 +0200

    Scheduler. Connection state added to class Client

--- Schelduer/Scheduler/Client.h
+++ Schelduer/Scheduler/Client.h
@@ -7,6 +7,13 @@ class Client
 {
     public:
         Client();
+
+        enum class ConnectionState { None, WaitForOkMessageAfterSendStatus };
+        ConnectionState GetConnectionState() { return workerConnectionState; }
+        void SetConnectionState(ConnectionState state) { workerConnectionState = state; }
+
+    private:
+        ConnectionState workerConnectionState = ConnectionState::None;
 };
 
 #endif // CLIENT_H
