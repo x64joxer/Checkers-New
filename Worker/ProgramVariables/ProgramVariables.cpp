d64be470b3863375e435854e6d936a54ff0e024f
Roman Nowak <roman.nowak.1984@gmail.com>
2016-10-05 16:49:33 +0200

    Worker. Trace flags added.

--- Worker/ProgramVariables/ProgramVariables.cpp
+++ Worker/ProgramVariables/ProgramVariables.cpp
@@ -29,3 +29,132 @@ unsigned int ProgramVariables::GetMaxTimeoutForMessageResponse()
 {
     return 1000;
 }
+
+//Traces
+
+bool ProgramVariables::GetTraceFlagForClass_Board()
+{
+    return traceFlagForClass_Board;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_Pawn()
+{
+    return traceFlagForClass_Pawn;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_PawnPos()
+{
+    return traceFlagForClass_PawnPos;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_PossibleMoves()
+{
+    return traceFlagForClass_PossibleMoves;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_ThreadIASimpleBoard()
+{
+    return traceFlagForClass_ThreadIASimpleBoard;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_IAPossibleMoves()
+{
+    return traceFlagForClass_IAPossibleMoves;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_Peers()
+{
+    return traceFlagForClass_Peers;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_QueueTimer()
+{
+    return traceFlagForClass_QueueTimer;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_SharedMap()
+{
+    return traceFlagForClass_SharedMap;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_SharedPtrSet()
+{
+    return traceFlagForClass_SharedPtrSet;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_SharedPtrList()
+{
+    return traceFlagForClass_SharedPtrList;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_ServerState()
+{
+    return traceFlagForClass_ServerState;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_TCPSocket()
+{
+    return traceFlagForClass_TCPSocket;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_TCPSocketBody()
+{
+    return traceFlagForClass_TCPSocketBody;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_ThreadIA()
+{
+    return traceFlagForClass_ThreadIA;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_ThreadIABoardQueue()
+{
+    return traceFlagForClass_ThreadIABoardQueue;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_ThreadIAMove()
+{
+    return traceFlagForClass_ThreadIAMove;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_ThreadIATreeExpander()
+{
+    return traceFlagForClass_ThreadIATreeExpander;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_Message()
+{
+    return traceFlagForClass_Message;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_MessageCode()
+{
+    return traceFlagForClass_MessageCode;
+}
+
+bool ProgramVariables::GetTraceFlagForClass_Worker()
+{
+    return traceFlagForClass_Worker;
+}
+
+bool ProgramVariables::traceFlagForClass_Board = false;
+bool ProgramVariables::traceFlagForClass_Pawn = false;
+bool ProgramVariables::traceFlagForClass_PawnPos = false;
+bool ProgramVariables::traceFlagForClass_PossibleMoves = false;
+bool ProgramVariables::traceFlagForClass_ThreadIASimpleBoard = false;
+bool ProgramVariables::traceFlagForClass_IAPossibleMoves = false;
+bool ProgramVariables::traceFlagForClass_Peers = false;
+bool ProgramVariables::traceFlagForClass_QueueTimer = false;
+bool ProgramVariables::traceFlagForClass_SharedMap = false;
+bool ProgramVariables::traceFlagForClass_SharedPtrSet = false;
+bool ProgramVariables::traceFlagForClass_SharedPtrList = false;
+bool ProgramVariables::traceFlagForClass_ServerState = false;
+bool ProgramVariables::traceFlagForClass_TCPSocket = false;
+bool ProgramVariables::traceFlagForClass_TCPSocketBody = false;
+bool ProgramVariables::traceFlagForClass_ThreadIA = false;
+bool ProgramVariables::traceFlagForClass_ThreadIABoardQueue = false;
+bool ProgramVariables::traceFlagForClass_ThreadIAMove = false;
+bool ProgramVariables::traceFlagForClass_ThreadIATreeExpander = false;
+bool ProgramVariables::traceFlagForClass_Message = false;
+bool ProgramVariables::traceFlagForClass_MessageCode = false;
+bool ProgramVariables::traceFlagForClass_Worker = false;
--- Worker/ProgramVariables/ProgramVariables.h
+++ Worker/ProgramVariables/ProgramVariables.h
@@ -14,6 +14,51 @@ class ProgramVariables
         static unsigned int GetTimeReserveToSendBestResultToScheduler();
         static unsigned int GetMaxTimeoutForMessageResponse();
 
+        //Traces flags
+        static bool GetTraceFlagForClass_Board();
+        static bool GetTraceFlagForClass_Pawn();
+        static bool GetTraceFlagForClass_PawnPos();
+        static bool GetTraceFlagForClass_PossibleMoves();
+        static bool GetTraceFlagForClass_ThreadIASimpleBoard();
+        static bool GetTraceFlagForClass_ThreadIABoardQueue();
+        static bool GetTraceFlagForClass_IAPossibleMoves();
+        static bool GetTraceFlagForClass_Peers();
+        static bool GetTraceFlagForClass_QueueTimer();
+        static bool GetTraceFlagForClass_SharedMap();
+        static bool GetTraceFlagForClass_SharedPtrSet();
+        static bool GetTraceFlagForClass_SharedPtrList();
+        static bool GetTraceFlagForClass_ServerState();
+        static bool GetTraceFlagForClass_TCPSocket();
+        static bool GetTraceFlagForClass_TCPSocketBody();
+        static bool GetTraceFlagForClass_ThreadIA();
+        static bool GetTraceFlagForClass_ThreadIAMove();
+        static bool GetTraceFlagForClass_ThreadIATreeExpander();
+        static bool GetTraceFlagForClass_Message();
+        static bool GetTraceFlagForClass_MessageCode();
+        static bool GetTraceFlagForClass_Worker();
+
+    private:
+        static bool traceFlagForClass_Board;
+        static bool traceFlagForClass_Pawn;
+        static bool traceFlagForClass_PawnPos;
+        static bool traceFlagForClass_PossibleMoves;
+        static bool traceFlagForClass_ThreadIASimpleBoard;
+        static bool traceFlagForClass_IAPossibleMoves;
+        static bool traceFlagForClass_Peers;
+        static bool traceFlagForClass_QueueTimer;
+        static bool traceFlagForClass_SharedMap;
+        static bool traceFlagForClass_SharedPtrList;
+        static bool traceFlagForClass_SharedPtrSet;
+        static bool traceFlagForClass_ServerState;
+        static bool traceFlagForClass_TCPSocket;
+        static bool traceFlagForClass_TCPSocketBody;
+        static bool traceFlagForClass_ThreadIA;
+        static bool traceFlagForClass_ThreadIABoardQueue;
+        static bool traceFlagForClass_ThreadIAMove;
+        static bool traceFlagForClass_ThreadIATreeExpander;
+        static bool traceFlagForClass_Message;
+        static bool traceFlagForClass_MessageCode;
+        static bool traceFlagForClass_Worker;
 };
 
 #endif // PROGRAMVARIABLES_H
